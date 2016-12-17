87048e91f45d4c8a6840e2e978f19e39bf0faba4
Naoise Golden <naoise.golden@gmail.com>
2016-12-17 15:09:55 +0100

    Iteration 1: ingredients

--- src/pizza.js
+++ src/pizza.js
@@ -1,1 +1,12 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
+function onClickIngredient(buttonClass, ingredientClass) {
+  $(buttonClass).on('click', function() {
+    $(ingredientClass).fadeToggle();
+  });
+}
+
+onClickIngredient('.btn-pepperonni', '.pep');
+onClickIngredient('.btn-mushrooms', '.mushroom');
+onClickIngredient('.btn-green-peppers', '.green-pepper');
