9cb90a86aed8642f3f558ed8a6eb3f3c32c5866b
Naoise Golden <naoise.golden@gmail.com>
2016-12-17 16:25:09 +0100

    Iteration 5: update ingredient itemization

--- src/pizza.js
+++ src/pizza.js
@@ -5,24 +5,26 @@ function updatePrice() {
   $('.price strong').text('$' + price);
 }
 
-function onClickIngredient(buttonClass, ingredientClass) {
+function onClickIngredient(buttonClass, ingredientClass, priceClass) {
   $(buttonClass).on('click', function() {
     $(this).toggleClass('active');
     $(ingredientClass).fadeToggle();
+    $(priceClass).slideToggle();
 
     $(this).hasClass('active') ? price += 1 : price -= 1;
     updatePrice();
   });
 }
 
-onClickIngredient('.btn-pepperonni', '.pep');
-onClickIngredient('.btn-mushrooms', '.mushroom');
-onClickIngredient('.btn-green-peppers', '.green-pepper');
+onClickIngredient('.btn-pepperonni', '.pep', '.price-pepperonni');
+onClickIngredient('.btn-mushrooms', '.mushroom', '.price-mushrooms');
+onClickIngredient('.btn-green-peppers', '.green-pepper', '.price-green-peppers');
 
 
 $('.btn-sauce').on('click', function() {
   $(this).toggleClass('active');
   $('.sauce').toggleClass('sauce-white');
+  $('.price-sauce').fadeToggle();
 
   $(this).hasClass('active') ? price += 3 : price -= 3;
   updatePrice();
@@ -31,6 +33,7 @@ $('.btn-sauce').on('click', function() {
 $('.btn-crust').on('click', function() {
   $(this).toggleClass('active');
   $('.crust').toggleClass('crust-gluten-free');
+  $('.price-crust').fadeToggle();
 
   $(this).hasClass('active') ? price += 5 : price -= 5;
   updatePrice();
