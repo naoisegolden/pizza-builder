10d06924fa91e2f96fa64372e0b8d9e89c354fae
Naoise Golden <naoise.golden@gmail.com>
2016-12-17 15:27:00 +0100

    Iteration 4: update price

--- src/pizza.js
+++ src/pizza.js
@@ -1,10 +1,17 @@
-// Write your Pizza Builder JavaScript in this file.
+var price = 21;
+updatePrice();
 
+function updatePrice() {
+  $('.price strong').text('$' + price);
+}
 
 function onClickIngredient(buttonClass, ingredientClass) {
   $(buttonClass).on('click', function() {
     $(this).toggleClass('active');
     $(ingredientClass).fadeToggle();
+
+    $(this).hasClass('active') ? price += 1 : price -= 1;
+    updatePrice();
   });
 }
 
@@ -16,9 +23,15 @@ onClickIngredient('.btn-green-peppers', '.green-pepper');
 $('.btn-sauce').on('click', function() {
   $(this).toggleClass('active');
   $('.sauce').toggleClass('sauce-white');
+
+  $(this).hasClass('active') ? price += 3 : price -= 3;
+  updatePrice();
 });
 
 $('.btn-crust').on('click', function() {
   $(this).toggleClass('active');
   $('.crust').toggleClass('crust-gluten-free');
+
+  $(this).hasClass('active') ? price += 5 : price -= 5;
+  updatePrice();
 });
